997cd2a5ef0de2e6e1155fc91d92d7d9c5a268a2
snakepp <cj10jose1@gmail.com>
2014-06-14 18:19:04 -0500

    create tournament project

---
+++ Tournament/src/com/dota/tournament/Register.java
@@ -0,0 +1,74 @@
+package com.dota.tournament;
+
+
+import com.vaadin.data.validator.EmailValidator;
+import com.vaadin.event.MouseEvents.ClickEvent;
+import com.vaadin.event.MouseEvents.ClickListener;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class Register extends VerticalLayout{
+
+	private FormLayout form;
+	private TextField user,email;
+	private PasswordField password;
+	private PasswordField verifyPassword;
+	private Button register;
+	private Notification registerNotif;
+	
+	public Register(){
+		form = new FormLayout();
+		user = new TextField("User", "");
+		user.setRequired(true);
+		user.setInputPrompt("your nickname");
+		user.setInvalidAllowed(false);
+		email = new TextField("Email");
+		email.setInputPrompt("email@gmail.com");
+		email.setInvalidAllowed(false);
+		email.addValidator(new EmailValidator("email must be a valid email address"));
+		email.setRequired(true);
+		password = new PasswordField("Password");
+		password.setRequired(true);
+		password.setInvalidAllowed(false);
+		password.setWidth("300px");
+		verifyPassword = new PasswordField("Verify Password");
+		setCaption("register new member");
+		setSpacing(true);
+		setMargin(new MarginInfo(true,true,true,false));
+		setSizeUndefined();
+		
+		register = new Button("Register");
+		register.addClickListener(RegisterListener());
+		
+		form.addStyleName("outlined");
+		form.setSizeFull();
+		form.setSpacing(true);
+
+		form.addComponent(user);
+		form.addComponent(email);
+		form.addComponent(password);
+		form.addComponent(verifyPassword);
+	    form.addComponent(register);
+	    form.setComponentAlignment(register, Alignment.MIDDLE_CENTER);
+	    addComponent(form);
+	}
+	
+	public Button.ClickListener RegisterListener(){
+		return new Button.ClickListener() {
+
+			@Override
+			public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+				Notification.show("Register Successful!","the register was completed successfully, to activate your account follow the mail instructions that we sent to your mail.",
+		                  Notification.Type.TRAY_NOTIFICATION);
+			}
+		};
+	}
+}
---
+++ Tournament/src/com/dota/tournament/TournamentUI.java
@@ -0,0 +1,150 @@
+package com.dota.tournament;
+
+import java.io.File;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.FileResource;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+@Theme("my-chameleon")
+public class TournamentUI extends UI {
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = TournamentUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
+
+	final GridLayout mainLayout = new GridLayout();
+	final VerticalLayout presentationLayout = new VerticalLayout();
+	final HorizontalLayout headLayout = new HorizontalLayout();
+	final VerticalLayout menuLayout = new VerticalLayout();
+	final VerticalLayout bodyLayout = new VerticalLayout();
+	final VerticalLayout notificationLayout = new VerticalLayout();
+	final VerticalLayout creditsLayout = new VerticalLayout();
+	final Window subWindow = new Window();
+	
+	@Override
+	protected void init(VaadinRequest request) {
+		mainLayout.addStyleName("outlined");
+		mainLayout.setSizeFull();
+		
+		
+		Label verify = new Label("Verify Layout");
+		Label verify2 = new Label("Verify Layout");
+		Label verify3 = new Label("Verify Layout");
+		Label verify4 = new Label("Verify Layout");
+		Label verify5 = new Label("Verify Layout");
+		Label verify6 = new Label("Verify Layout");
+		
+		headLayout.addComponent(verify);
+		
+		// Find the application directory
+		String basepath = VaadinService.getCurrent()
+		                  .getBaseDirectory().getAbsolutePath();
+		// Image as a file resource
+		FileResource resource = new FileResource(new File(basepath +
+		                        "/WEB-INF/images/logo/head.jpg"));
+		// Show the image in the application
+		Image image = new Image("Image from file", resource);
+		
+//		headLayout.addComponent(image);
+		headLayout.addComponent(verify);
+		presentationLayout.addComponent(verify6);
+		
+		Button register = new Button("Register");
+		register.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				//make something here
+				subWindow.setContent(new Register());
+				addWindow(subWindow);
+			}
+		});
+		
+		
+		presentationLayout.addComponent(register);
+		menuLayout.addComponent(verify2);
+		bodyLayout.addComponent(verify3);
+		notificationLayout.addComponent(verify4);
+		creditsLayout.addComponent(verify5);
+		
+		
+		
+		generateBorderLayout();
+		setContent(mainLayout);
+	}
+
+	private void generateBorderLayout() {
+		mainLayout.setRows(3);
+		mainLayout.setColumns(3);
+
+		
+//        mainLayout.addComponent(headLayout, 0, 0, 2, 0);
+		headLayout.setMargin(true);
+		presentationLayout.setMargin(true);
+		menuLayout.setMargin(true);
+		bodyLayout.setMargin(true);
+		notificationLayout.setMargin(true);
+		creditsLayout.setMargin(true);
+		
+		mainLayout.addComponent(headLayout, 0, 0);
+		mainLayout.addComponent(presentationLayout, 2, 0);
+        mainLayout.addComponent(menuLayout, 0, 1);
+        mainLayout.addComponent(bodyLayout, 1, 1);
+        mainLayout.addComponent(notificationLayout, 2, 1);
+        mainLayout.addComponent(creditsLayout, 0, 2, 2, 2);
+
+        mainLayout.setRowExpandRatio(0, 1.0f);
+        mainLayout.setRowExpandRatio(1, 5.0f);
+        mainLayout.setRowExpandRatio(2, 1.0f);
+
+        mainLayout.setColumnExpandRatio(0, 1.0f);
+        mainLayout.setColumnExpandRatio(1, 4.0f);
+        mainLayout.setColumnExpandRatio(2, 1.0f);
+    }
+	
+	@Deprecated
+	private void demo(){
+		final HorizontalLayout headLayout = new HorizontalLayout();
+		final VerticalLayout mainLayout = new VerticalLayout();
+		final VerticalLayout bodyLayout = new VerticalLayout();
+		
+			
+		mainLayout.setMargin(true);
+		mainLayout.addStyleName("outlined");		
+		headLayout.setMargin(true);
+		headLayout.addStyleName("outlined");
+		
+		
+		Button login = new Button("Login");
+		login.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				//make something here
+				bodyLayout.addComponent(new Label("pepe"));
+			}
+		});
+		
+		
+		
+		
+		mainLayout.addComponent(headLayout);
+		mainLayout.addComponent(bodyLayout);
+		setContent(mainLayout);
+	}
+}
